e914bb1a303b944527fd68a63c2e4c4a6476831d
GabrielleHandy <gehandy123@gmail.com>
2023-10-23 23:47:46 -0400

    Added sign up component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent,
+  
   ],
   imports: [
     BrowserModule,

d8de350c9cd424f3cfac59ab6af3700b5c7b0fd6
GabrielleHandy <gehandy123@gmail.com>
2023-10-24 10:34:44 -0400

    made signup basic format

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
-
+  password = '';
+  email = '';
+  username = '';
+  
+  signup() {
+    console.log('signup');
+  
+  }
 }

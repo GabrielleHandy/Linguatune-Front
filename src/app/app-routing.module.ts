250a663480f6a8b1f8949335c9ce5045da382cdd
GabrielleHandy <91081738+GabrielleHandy@users.noreply.github.com>
2023-10-24 23:52:05 +0000

    Added basic card page with basic layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SettingsComponent } from './settings/settings.component';
+import { StudypageComponent } from './studypage/studypage.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
   {path:'signup', component: SignupComponent},
   {path:'dashboard', component: DashboardComponent},
-  {path:"settings", component: SettingsComponent}
+  {path:"settings", component: SettingsComponent},
+  {path: "studypage", component: StudypageComponent}
 ];
 
 @NgModule({
